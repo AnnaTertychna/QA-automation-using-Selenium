c0a4d385ca070968c3f9f6dd36e9aee0ab76a44a
AnnaTertychna <anna1704nl@gmail.com>
2018-02-28 22:41:37 +0200

    logout

--- homework2/src/main/java/com/anna/homework2/AdminPanelLogin.java
+++ homework2/src/main/java/com/anna/homework2/AdminPanelLogin.java
@@ -1,6 +1,8 @@
 package com.anna.homework2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AdminPanelLogin {
@@ -11,9 +13,27 @@ public class AdminPanelLogin {
 
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
 
-        Thread.sleep(5000);
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("webinar.test@gmail.com");
 
-        driver.close();
+        WebElement password = driver.findElement(By.name("passwd"));
+        password.sendKeys("Xcg7299bnSmMuRLp9ITw");
+
+        WebElement button = driver.findElement(By.name("submitLogin"));
+        button.submit();
+
+        Thread.sleep(3000);
+
+        WebElement employeeInfo = driver.findElement(By.id("employee_infos"));
+        employeeInfo.click();
+
+        Thread.sleep(3000);
+
+        WebElement logOut = driver.findElement(By.id("header_logout"));
+        logOut.click();
+
+        Thread.sleep(3000);
+
         driver.quit();
     }
 }
