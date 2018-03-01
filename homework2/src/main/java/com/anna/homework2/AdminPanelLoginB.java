3a4ce9de1a30d551f0c64d57ed6f6ee9e4da3795
AnnaTertychna <anna1704nl@gmail.com>
2018-03-01 22:45:54 +0200

    home work 2; task 2/b

--- homework2/src/main/java/com/anna/homework2/AdminPanelLoginB.java
+++ homework2/src/main/java/com/anna/homework2/AdminPanelLoginB.java
@@ -5,11 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-
 public class AdminPanelLoginB {
     public static void main(String[] arguments) throws Exception {
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver-2.33.exe");
@@ -29,72 +24,47 @@ public class AdminPanelLoginB {
         button.submit();
         Thread.sleep(3000);
 
-        WebElement dashboard = driver.findElement (By.className("title"));
-        dashboard.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.className("title"));
 
-        WebElement orders = driver.findElement(By.id("subtab-AdminParentOrders"));
-        orders.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminParentOrders"));
 
-        WebElement catalog = driver.findElement(By.id("subtab-AdminCatalog"));
-        catalog.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminCatalog"));
 
-        WebElement client = driver.findElement(By.xpath("//span[contains(text(),'Клиенты')]"));
-        client.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.xpath("//span[contains(text(),'Клиенты')]"));
 
-        WebElement custemerServis = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
-        custemerServis.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminParentCustomerThreads"));
 
-        WebElement statistics = driver.findElement(By.id("subtab-AdminStats"));
-        statistics.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminStats"));
 
-        WebElement modules = driver.findElement(By.id("subtab-AdminParentModulesSf"));
-        modules.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminParentModulesSf"));
 
-        WebElement design = driver.findElement(By.xpath("//span[contains(text(),'Design')]"));
-        design.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.xpath("//span[contains(text(),'Design')]"));
 
-        WebElement schipping = driver.findElement(By.id("subtab-AdminParentShipping"));
-        schipping.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminParentShipping"));
 
-        WebElement payment = driver.findElement(By.id("subtab-AdminParentPayment"));
-        payment.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminParentPayment"));
 
-        WebElement international = driver.findElement(By.id("subtab-AdminInternational"));
-        international.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminInternational"));
 
-        WebElement schopParametrs = driver.findElement(By.id("subtab-ShopParameters"));
-        schopParametrs.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-ShopParameters"));
 
-        WebElement configuration = driver.findElement(By.id("subtab-AdminAdvancedParameters"));
-        configuration.click();
-        Thread.sleep(3000);
-        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        processMenuItem(driver, By.id("subtab-AdminAdvancedParameters"));
 
         driver.quit();
+    }
+
+    private static void processMenuItem(WebDriver driver, By by) throws InterruptedException {
+        WebElement element = driver.findElement(by);
+        element.click();
+        Thread.sleep(3000);
+        String sectionNameBefore = driver.findElement(By.tagName("h2")).getText();
+        System.out.println(sectionNameBefore);
+
+        driver.navigate().refresh();
+        String sectionNameAfter = driver.findElement(By.tagName("h2")).getText();
+        if (!sectionNameBefore.equals(sectionNameAfter)) {
+            System.out.println("Unexpected section name: " + sectionNameAfter + ", expected: " + sectionNameBefore);
+        }
     }
 
 }
