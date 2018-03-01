d447dd1049945617ee096f78c0c390f3ff42f7a5
AnnaTertychna <anna1704nl@gmail.com>
2018-03-01 22:24:24 +0200

    home work 2; task 2/a

--- homework2/src/main/java/com/anna/homework2/AdminPanelLoginB.java
+++ homework2/src/main/java/com/anna/homework2/AdminPanelLoginB.java
@@ -32,26 +32,67 @@ public class AdminPanelLoginB {
         WebElement dashboard = driver.findElement (By.className("title"));
         dashboard.click();
         Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
 
         WebElement orders = driver.findElement(By.id("subtab-AdminParentOrders"));
         orders.click();
         Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
 
         WebElement catalog = driver.findElement(By.id("subtab-AdminCatalog"));
         catalog.click();
         Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
 
         WebElement client = driver.findElement(By.xpath("//span[contains(text(),'Клиенты')]"));
         client.click();
         Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
 
         WebElement custemerServis = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
         custemerServis.click();
         Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
 
         WebElement statistics = driver.findElement(By.id("subtab-AdminStats"));
         statistics.click();
         Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement modules = driver.findElement(By.id("subtab-AdminParentModulesSf"));
+        modules.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement design = driver.findElement(By.xpath("//span[contains(text(),'Design')]"));
+        design.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement schipping = driver.findElement(By.id("subtab-AdminParentShipping"));
+        schipping.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement payment = driver.findElement(By.id("subtab-AdminParentPayment"));
+        payment.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement international = driver.findElement(By.id("subtab-AdminInternational"));
+        international.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement schopParametrs = driver.findElement(By.id("subtab-ShopParameters"));
+        schopParametrs.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        WebElement configuration = driver.findElement(By.id("subtab-AdminAdvancedParameters"));
+        configuration.click();
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
 
         driver.quit();
     }
