1da9c30aeaadcbafbe41a873e70659cfc08529b1
AnnaTertychna <anna1704nl@gmail.com>
2018-03-18 14:55:49 +0200

    Homework 4B

--- homework4/src/test/java/com/anna/homework4/Homework4.java
+++ homework4/src/test/java/com/anna/homework4/Homework4.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.text.DecimalFormat;
@@ -17,54 +19,64 @@ import java.util.Random;
 import java.util.UUID;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class Homework4 {
+    private static final String URL = "http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/";
 
     private final String productName = UUID.randomUUID().toString();
     private final int quantity = new Random().nextInt(100) + 1;
     private final double price = Double.parseDouble(new DecimalFormat("#.##")
             .format(new Random().nextFloat() * 100 + 0.1));
+    private WebDriver driver;
 
-    @Test(dependsOnMethods = "testA")
-    public void testB(){
-        WebDriver driver = getWebDriver();
+    @DataProvider
+    public Object[][] getData() {
+        return new Object[][]{{"webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw"}};
+    }
 
-        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+    @BeforeMethod
+    @Parameters({ "browser" })
+    public void setUp(String browser) {
+        driver = getWebDriver(browser);
+    }
 
-        login(driver);
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test(dependsOnMethods = "testA", dataProvider = "getData")
+    public void testB(String login, String password){
+        driver.get(URL);
+
+        login(driver, login, password);
 
         openProductsPage(driver);
 
-        waitForElement(driver, By.tagName("h2"));
-
-        driver.findElement(By.name("filter_column_name")).sendKeys(productName);
-        driver.findElement(By.name("products_filter_submit")).click();
+        WebElement nameFilter = driver.findElement(By.name("filter_column_name"));
+        nameFilter.clear();
+        nameFilter.sendKeys(productName);
+        driver.findElement(By.name("products_filter_submit")).submit();
         waitForElement(driver, By.linkText(productName));
 
         assertEquals(driver.findElements(By.linkText(productName)).size(), 1);
 
-        String priceText = driver.findElement(By.xpath("//a[text() = '" +productName + "']/../../td[6]/a")).getText();
+        String priceText = driver.findElement(By.xpath("//a[text() = '" + productName + "']/../../td[6]/a")).getText();
         assertEquals(price, Double.parseDouble(priceText.split(" ")[0].replace(",", ".")));
 
-        String quantityText = driver.findElement(By.xpath("//a[text() = '" +productName + "']/../../td[contains(@class, 'product-sav-quantity')]/a"))
+        String quantityText = driver.findElement(By.xpath("//a[text() = '" + productName
+                + "']/../../td[contains(@class, 'product-sav-quantity')]/a"))
                 .getText();
         assertEquals(quantity, Integer.parseInt(quantityText));
-
-        driver.quit();
     }
 
-    @Test
-    public void testA(){
-        WebDriver driver = getWebDriver();
+    @Test(dataProvider = "getData")
+    public void testA(String login, String password){
+        driver.get(URL);
 
-        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
-
-        login(driver);
+        login(driver, login, password);
 
         openProductsPage(driver);
-
-        waitForElement(driver, By.tagName("h2"));
 
         driver.findElement(By.id("page-header-desc-configuration-add")).click();
 
@@ -73,7 +85,6 @@ public class Homework4 {
         driver.findElement(byProductName).sendKeys(productName);
 
         driver.findElement(By.id("tab_step3")).click();
-
         By byAmount = By.id("form_step3_qty_0");
         waitForElement(driver, byAmount);
         driver.findElement(byAmount).sendKeys(String.valueOf(quantity));
@@ -90,11 +101,9 @@ public class Homework4 {
         driver.findElement (By.className("js-btn-save")).click();
         waitForElement(driver,By.className("growl-message"));
         driver.findElement(By.className("growl-close")).click();
-
-        driver.quit();
     }
 
-    private static void openProductsPage(WebDriver driver) {
+    private void openProductsPage(WebDriver driver) {
         By byCatalog = By.id("subtab-AdminCatalog");
         waitForElement(driver, byCatalog);
         Actions actions = new Actions(driver);
@@ -103,50 +112,55 @@ public class Homework4 {
         waitForElement(driver, byProducts);
         WebElement products = driver.findElement(byProducts);
         products.click();
+        waitForElement(driver, By.tagName("h2"));
     }
 
-    private static void login(WebDriver driver) {
+    private void login(WebDriver driver, String login, String password) {
         By byEmail = By.name("email");
         waitForElement(driver, byEmail);
 
-        WebElement email = driver.findElement(byEmail);
-        email.sendKeys("webinar.test@gmail.com");
+        driver.findElement(byEmail).sendKeys(login);
+        driver.findElement(By.name("passwd")).sendKeys(password);
 
-        WebElement password = driver.findElement(By.name("passwd"));
-        password.sendKeys("Xcg7299bnSmMuRLp9ITw");
-
-        WebElement button = driver.findElement(By.name("submitLogin"));
-        button.submit();
+        driver.findElement(By.name("submitLogin")).submit();
     }
 
-    private static WebElement waitForElement(WebDriver driver, By by) {
+    private WebElement waitForElement(WebDriver driver, By by) {
         WebDriverWait waitCatalog = new WebDriverWait(driver, 10);
         waitCatalog.until(ExpectedConditions.visibilityOfElementLocated(by));
         return driver.findElement(by);
     }
 
-    private static WebDriver getWebDriver() {
-        String driver = "chromedriver-2.35.exe";
-        String property = "webdriver.chrome.driver";
-
-        System.setProperty(property,  new File(Homework4.class
-                .getResource("/" + getOsDirectory() + "/" + driver).getFile()).getPath());
-
-        RemoteWebDriver browserDriver = new ChromeDriver();
-        EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(browserDriver);
-        eventFiringWebDriver.register(new MyWebDriverEventListener());
-        return eventFiringWebDriver;
-    }
-
-    private static String getOsDirectory() {
-        String osName = System.getProperty("os.name");
-        String osDirectory = "";
-        if (osName.contains("Win")) {
-            osDirectory = "windows";
-        } else {
-            throw new RuntimeException(osName + " is not supported");
+    private WebDriver getWebDriver(String browser) {
+        RemoteWebDriver browserDriver = null;
+        if (browser.toLowerCase().contains("chrome")) {
+            System.setProperty("webdriver.chrome.driver", getDriverPath("chromedriver-2.35.exe"));
+            browserDriver = new ChromeDriver();
+        } else if (browser.toLowerCase().contains("firefox")) {
+            System.setProperty("webdriver.gecko.driver", getDriverPath("geckodriver-0.20.0.exe"));
+            browserDriver = new FirefoxDriver();
+        } else if (browser.toLowerCase().contains("explorer")) {
+            System.setProperty("webdriver.ie.driver", getDriverPath("IEDriverServer-3.8.exe"));
+            browserDriver = new InternetExplorerDriver();
         }
-        return osDirectory;
+        if (browserDriver != null) {
+            EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(browserDriver);
+            eventFiringWebDriver.register(new MyWebDriverEventListener());
+            return eventFiringWebDriver;
+        }
+        throw new RuntimeException("Browser" + browser + " is not supported");
     }
 
+    private String getDriverPath(String driverExec) {
+        return new File(Homework4.class.getResource("/" + getOsDirectory() + "/" + driverExec)
+                .getFile()).getPath();
+    }
+
+    private String getOsDirectory() {
+        String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().contains("win")) {
+            return "windows";
+        }
+        throw new RuntimeException(osName + " is not supported");
+    }
 }
